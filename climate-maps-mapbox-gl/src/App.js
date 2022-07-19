11176c7f75c55ca1e6a7b41d08d7808fdffb423f
Gagan Nanu <28942499+GaganNanu@users.noreply.github.com>
2022-07-19 10:02:39 -0400

    Rename class to className

--- climate-maps-mapbox-gl/src/App.js
+++ climate-maps-mapbox-gl/src/App.js
@@ -4,28 +4,28 @@ import Map from './Map';
 //import Chart from './Charts';
 function App() {
   return (
-    <div class="container-flex">
-    <div class="row">
-    <div class="col-12 m-3">
+    <div className="container-flex">
+    <div className="row">
+    <div className="col-12 m-3">
       <h1>Historical CO2 Emissions</h1>
       <p>decanal CO2 emissions across different sources, by census tract</p>
       </div>
     </div>
-    <div class="row">
-      <div class="col-8 h-75">
+    <div className="row">
+      <div className="col-8 h-75">
       <Map></Map>
     </div>
-      <div class="col-4">
-       <div class="row">
-          <div class="col-12">
+      <div className="col-4">
+       <div className="row">
+          <div className="col-12">
               <h2>Other Charts</h2>
           </div>
         </div>
-        <div class="row">
-          <div class="col-6">
+        <div className="row">
+          <div className="col-6">
               <h2>Chart1</h2>
           </div>
-          <div class="col-6">
+          <div className="col-6">
               <h2>Chart2</h2>
           </div>
         </div>
