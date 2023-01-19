665e906b24f787bc7b74adbd33d7c2bd10e1fcfa
ezra <edgertej@ucmail.uc.edu>
2023-01-19 13:16:38 -0600

    updating about, copy edits

--- climate-maps-mapbox-gl/src/About.js
+++ climate-maps-mapbox-gl/src/About.js
@@ -2,10 +2,12 @@ export default function About() {
     return (
         <div className="home-content">
             <p><h3>Rob Gioielli: Project Director</h3><em>Associate Professor of History University of Cincinnati Blue Ash College</em></p>
-            <p><h3>Amy Koshoffer</h3></p>
-            <p><h3>Woolack Kim</h3></p>
-            <p><h3>Ezra Edgerton</h3></p>
-            <p><h3>Jennifer Latessa</h3></p>
+            
+            <p><h3>Woo-Lack Kim: Geographic Modeling Lead</h3><em>PhD student, School of Planning at DAAP</em></p>
+            <p><h3>Ezra Edgerton : Data Visualization</h3><em>Data Scientist, UC Digital Scholarship Center</em></p>
+            <p><h3>Amy Koshoffer: Consulting Librarian</h3><em>Assistant Director of Research and Data Services , UC Libraries</em></p>
+            <p><h3>Jennifer Latessa : Mapping Specialist</h3><em>Graduate Student</em></p>
+
             <p><h3>Gagan Kumar Reddy Nanu: Software Engineer</h3><em>Graduate Student, MEng in Computer Science</em></p>
         </div>
     )
