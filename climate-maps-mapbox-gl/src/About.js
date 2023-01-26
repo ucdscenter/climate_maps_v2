4507af740567ceb45c67b90954fe2b295e145558
ezra <edgertej@ucmail.uc.edu>
2023-01-26 10:31:47 -0600

    updating copy

--- climate-maps-mapbox-gl/src/About.js
+++ climate-maps-mapbox-gl/src/About.js
@@ -6,9 +6,8 @@ export default function About() {
             <p><h3>Woo-Lack Kim: Geographic Modeling Lead</h3><em>PhD student, School of Planning at DAAP</em></p>
             <p><h3>Ezra Edgerton : Data Visualization</h3><em>Data Scientist, UC Digital Scholarship Center</em></p>
             <p><h3>Amy Koshoffer: Consulting Librarian</h3><em>Assistant Director of Research and Data Services , UC Libraries</em></p>
-            <p><h3>Jennifer Latessa : Mapping Specialist</h3><em>Graduate Student</em></p>
-
             <p><h3>Gagan Kumar Reddy Nanu: Software Engineer</h3><em>Graduate Student, MEng in Computer Science</em></p>
+            <p>Special thanks to Jenny Latessa, who made significant contributions to the the previous incarnation of this project.</p>
         </div>
     )
 }
--- climate-maps-mapbox-gl/src/Home.js
+++ climate-maps-mapbox-gl/src/Home.js
@@ -13,16 +13,13 @@ export default function Home() {
                 What’s your carbon footprint?
             </h1>
             <p>
-                This is the question many Americans have been asking themselves for the past two decades, as we try to address the causes of human-induced climate change, particularly our consumption of fossil fuels.
-
-                Recently, activists and experts have begun highlighting how not all carbon footprints are created equal. The biggest sources of greenhouse gas are on the production side, particularly agriculture and heavy industry. But consumer “footprints” vary widely as well.
+            This is the question many Americans have been asking themselves for the past two decades, as we try to address the causes of human-induced climate change, particularly our consumption of fossil fuels. Recently, activists and experts have begun highlighting how not all carbon footprints are created equal. The biggest sources of greenhouse gas are on the production side, particularly agriculture and heavy industry. But consumer “footprints” vary widely as well, especially across our major cities. Some folks have smaller apartments and use public transportation, while others live in large suburban homes and drive hundreds of miles a week.  
             </p>
             <p>
                 This website is a tool for you to see what the carbon footprint is in your community, and how that has changed over time. We also included demographic information, particularly family income, race and ethnicity, to help you consider how social factors shape our carbon consumption.
             </p>
             <p>
-                This project is sponsored by the University of Cincinnati Digital Scholarship Center. We encourage you to <a href="#" onClick={() => { navigate('/methods'); return false }}>explore our methodology</a> and <a href="#" onClick={() => { navigate('/about'); return false }}>learn more about our team</a>.
-            </p>
+                This website is a tool for you to see the average household carbon usage in your community, and how that has changed over time. But we also wanted to consider how social and demographic factors shape carbon usage, especially race. We examined a number of variables in our research, but for this current version we show how the percentage of White residents in a given metropolitan community corresponds to household carbon use.             </p>
             <hr></hr>
             <h1>How to Use This Site  </h1>
             <p>
@@ -49,6 +46,9 @@ export default function Home() {
  <h3><i>Full Data</i></h3>
  <p>On the bottom right is the full data set for each rendering. Each census tract has a “GEOID” number.  We know is not very helpful in identifying a specific location. We are working on linking this data to the map to make this section more usable.  </p>  
  <hr></hr>
+
+ <h5>Feedback? </h5>
+ <p>Email any feedback or specific questions and requests to gioielr@ucmail.uc.edu</p>
 </div >
     )
 }
